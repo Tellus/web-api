a449de267c80e43161921383c2749b6922278313
Morten Hartvigsen <m.hartvigsen@gmail.com>
2019-03-04 13:16:33 +0100

    Database now supports num of days

--- GirafRest/Migrations/20190304082320_alterSettingsToIncludeWeekMode.cs
+++ GirafRest/Migrations/20190304082320_alterSettingsToIncludeWeekMode.cs
@@ -9,11 +9,13 @@ namespace GirafRest.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<int>("weekDisplayMode", "Setting");
+            migrationBuilder.AddColumn<int>("weekDisplayNumOfDays", "Setting");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropColumn("weekDisplayMode", "Setting");
+            migrationBuilder.DropColumn("weekDisplayNumOfDays", "Setting");
         }
     }
 }
